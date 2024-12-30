6df3162c8b9d381aa854df3fe6fd0f16bb14eb2c
JurgenLB <77586573+JurgenLB@users.noreply.github.com>
2024-12-30 21:51:52 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,6 @@ const RFXShutterDynamicPlatform = require('./platform/RFXShutterDynamicPlatform'
 /*
  * Initializer function called when the plugin is loaded.
  */
-export default (api: API): void => {
+export default (api: typeof API): void => {
   api.registerPlatform(PLATFORM_NAME, RFXShutterDynamicPlatform);
 };
