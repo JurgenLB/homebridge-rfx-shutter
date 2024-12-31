335634dafc02887582196b41282496f364bf370e
JurgenLB <77586573+JurgenLB@users.noreply.github.com>
2024-12-31 07:11:07 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 const { API } = require('homebridge');
-const RFXShutterDynamicPlatform, { PLATFORM_NAME } = require('./platform/RFXShutterDynamicPlatform');
+const RFXShutterDynamicPlatform = require('./platform/RFXShutterDynamicPlatform');
 
 /*
  * Initializer function called when the plugin is loaded.
  */
 export default (api: typeof API): void => {
-  api.registerPlatform(PLATFORM_NAME, RFXShutterDynamicPlatform);
+  api.registerPlatform( { PLATFORM_NAME }, RFXShutterDynamicPlatform);
 };
